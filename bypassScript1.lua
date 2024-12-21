9eb449046705cf44a517c1da5533c29195bd8fef
POOHHUB <91407201+TEWIOCK@users.noreply.github.com>
2024-12-21 19:02:04 +0700

    Update and rename bypassScript.lua to bypassScript1.lua

--- bypassScript1.lua
+++ bypassScript1.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/xZPUHigh/Spectrum-Cloud/main/Loader.lua'))()
-wait(1)
+
 game:GetService("CoreGui").THUNDERZHUB:destroy()
 game:GetService("CoreGui").ImageButton:destroy()
